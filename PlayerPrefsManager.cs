02aaf1fc9272a3598d9cdcc8d2eabe46456a7b55
dwdxs <3199853261@qq.com>
2025-10-22 21:44:48 +0800

    增加了对List,Dictionary泛型类型的支持

--- PlayerPrefsManager.cs
+++ PlayerPrefsManager.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerPrefsManager
@@ -16,8 +19,9 @@ public class PlayerPrefsManager
     /// <returns></returns>
     public object Load(Type targetType, string keyName)
     {
-
+        //获取实例中的字段信息
         FieldInfo[] fieldInfos = targetType.GetFields();
+        //通过传入的目标类型创建的object实例，本质还是传入的类型
         object targetObject = Activator.CreateInstance(targetType);
 
         string loadKeyName;
@@ -26,37 +30,53 @@ public class PlayerPrefsManager
         {
             loadKeyName = keyName + "_" + fieldInfo.FieldType.Name + "_" + fieldInfo.Name;
 
-            LoadValue(fieldInfo, loadKeyName, targetObject);
+            fieldInfo.SetValue(targetObject, LoadValue(fieldInfo.FieldType, loadKeyName));
         }
         return targetObject;
     }
-    private void LoadValue(FieldInfo fieldInfo, string loadKeyName, object targetObject)
+    private object LoadValue(Type fieldType, string loadKeyName)
     {
-
-        Type dataType = fieldInfo.FieldType;
-
-        if (dataType == typeof(int))
+        if (fieldType == typeof(int))
         {
-            int value = PlayerPrefs.GetInt(loadKeyName);
-            fieldInfo.SetValue(targetObject, value);
+            return PlayerPrefs.GetInt(loadKeyName);
         }
-        if (dataType == typeof(string))
+        else if (fieldType == typeof(string))
         {
-            string value = PlayerPrefs.GetString(loadKeyName);
-            fieldInfo.SetValue(targetObject, value);
+            return PlayerPrefs.GetString(loadKeyName);
+        }
+        else if (fieldType == typeof(float))
+        {
+            return PlayerPrefs.GetFloat(loadKeyName);
+        }
+        else if (typeof(IList).IsAssignableFrom(fieldType))
+        {
+            int Count = PlayerPrefs.GetInt(loadKeyName + "_Count");
+            IList list = Activator.CreateInstance(fieldType) as IList;
+            for (int i = 0; i < Count; i++)
+            {
+                list.Add(LoadValue(fieldType.GetGenericArguments()[0], loadKeyName + i));
+            }
+            return list;
+        }
+        else if (typeof(IDictionary).IsAssignableFrom(fieldType))
+        {
+            int Count = PlayerPrefs.GetInt(loadKeyName + "_Count");
+            IDictionary dic = Activator.CreateInstance(fieldType) as IDictionary;
+            object key;
+            object value;
+            for (int i = 0; i < Count; i++)
+            {
+                key = LoadValue(fieldType.GetGenericArguments()[0], loadKeyName + "_key_" + i);
+                value = LoadValue(fieldType.GetGenericArguments()[1], loadKeyName + "_value_" + i);
+                dic.Add(key, value);
+            }
+            return dic;
+        }
+        else
+        {
+            return Load(fieldType, loadKeyName);
+        }
 
-        }
-        if (dataType == typeof(float))
-        {
-            float value = PlayerPrefs.GetFloat(loadKeyName);
-            fieldInfo.SetValue(targetObject, value);
-
-        }
-        if (dataType == typeof(bool))
-        {
-            bool value = PlayerPrefs.GetInt(loadKeyName) == 1 ? true : false;
-            fieldInfo.SetValue(targetObject, value);
-        }
     }
     /// <summary>
     /// 存储数据
@@ -81,6 +101,7 @@ public class PlayerPrefsManager
             saveKeyName = keyName + "_" + fieldInfo.FieldType.Name + "_" + fieldInfo.Name;
 
             SaveValue(dataValue, saveKeyName);
+
         }
 
     }
@@ -90,18 +111,57 @@ public class PlayerPrefsManager
         if (dataValue is int)
         {
             PlayerPrefs.SetInt(saveKeyName, (int)dataValue);
+
+            Debug.Log(saveKeyName);
         }
-        if (dataValue is string)
+        else if (dataValue is string)
         {
             PlayerPrefs.SetString(saveKeyName, dataValue.ToString());
+
+            Debug.Log(saveKeyName);
         }
-        if (dataValue is float)
+        else if (dataValue is float)
         {
             PlayerPrefs.SetFloat(saveKeyName, (float)dataValue);
+
+            Debug.Log(saveKeyName);
         }
-        if (dataValue is bool)
+        else if (dataValue is bool)
         {
             PlayerPrefs.SetInt(saveKeyName, (bool)dataValue ? 1 : 0);
+
+            Debug.Log(saveKeyName);
+        }
+        else if (typeof(IList).IsAssignableFrom(dataValue.GetType()))
+        {
+            IList list = dataValue as IList;
+            PlayerPrefs.SetInt(saveKeyName + "_Count", list.Count);
+            int index = 0;
+            foreach (object obj in list)
+            {
+                SaveValue(obj, saveKeyName + index);
+
+                index += 1;
+            }
+        }
+        else if (typeof(IDictionary).IsAssignableFrom(dataValue.GetType()))
+        {
+            IDictionary dic = dataValue as IDictionary;
+            PlayerPrefs.SetInt(saveKeyName + "_Count", dic.Count);
+
+            Debug.Log(saveKeyName + "_Count");
+
+            int index = 0;
+            foreach (object key in dic.Keys)
+            {
+                SaveValue(key, saveKeyName + "_key_" + index);
+                SaveValue(dic[key], saveKeyName + "_value_" + index);
+                index += 1;
+            }
+        }
+        else
+        {
+            Save(dataValue, saveKeyName);
         }
     }
 
